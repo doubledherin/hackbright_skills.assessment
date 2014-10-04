93bb7244415385b2a46bd5fa287bc8069cd50c2f
Wendy Dherin <wendydherin@gmail.com>
2014-10-03 18:12:30 -0700

    Implemented test for 'sum_numbers' function (passed)

--- tests_skills1.py
+++ tests_skills1.py
@@ -30,40 +30,9 @@ class TestListOperations(unittest.TestCase):
     def test_1_G_word_lengths(self):
         self.assertEqual(word_lengths(self.word_list), [4, 5, 3, 4, 7, 4, 4, 5, 4, 6, 3, 4])
 
+    def test_1_H_sum_numbers(self):
+        self.assertEqual(sum_numbers(self.number_list), 118)
 """
-
-    def test_1_I_inner_four_end(self):
-        self.assertEqual(inner_four_end(self.months), ['Jul', 'Aug', 'Sep',
-                                                        'Oct'])
-        self.assertEqual(inner_four_end(self.notes), ['Mi', 'Fa', 'So', 'La'])
-        self.assertEqual(inner_four_end(self.multiples), [12, 15, 18, 21])
-
-    def test_1_J_replace_head(self):
-        replace_head(self.months)
-        replace_head(self.notes)
-        replace_head(self.multiples)
-
-        self.assertEqual(self.months, [42, 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                                       'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
-        self.assertEqual(self.notes, [42, 'Re', 'Mi', 'Fa', 'So', 'La', 'Ti',
-                                      'Do'])
-        self.assertEqual(self.multiples, [42, 3, 6, 9, 12, 15, 18, 21, 24, 27])
-
-    def test_1_K_replace_third_and_last(self):
-        replace_third_and_last(self.months)
-        replace_third_and_last(self.notes)
-        replace_third_and_last(self.multiples)
-
-        self.assertEqual(self.months, ['Jan', 'Feb', 37, 'Apr', 'May', 'Jun',
-                                       'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 37])
-        self.assertEqual(self.notes, ['Do', 'Re', 37, 'Fa', 'So', 'La', 'Ti', 37])
-        self.assertEqual(self.multiples, [0, 3, 37, 9, 12, 15, 18, 21, 24, 37])
-
-    def test_1_L_replace_middle(self):
-        replace_middle(self.months)
-        replace_middle(self.notes)
-        replace_middle(self.multiples)
-
         self.assertEqual(self.months, ['Jan', 'Feb', 42, 37, 'Nov', 'Dec'])
         self.assertEqual(self.notes, ['Do', 'Re', 42, 37, 'Ti', 'Do'])
         self.assertEqual(self.multiples, [0, 3, 42, 37, 24, 27])
