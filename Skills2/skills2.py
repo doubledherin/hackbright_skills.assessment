ac67252fce78a7bde8fe14fb0e9652a22877170b
Wendy Dherin <wendydherin@gmail.com>
2014-10-11 21:51:18 -0700

    Finished Pirate translator

--- Skills2/skills2.py
+++ Skills2/skills2.py
@@ -126,26 +126,96 @@ Given a list of words, print the words in ascending order of length
 Bonus: do it on a file instead of the list provided
 Bonus: print the words in alphabetical order in ascending order of length
 """
-# Started at 5:15 STOPPED at 5:22, still work to do on this.
+# Took 18 minutes
 def word_length(words):
     d = {}
     
-    # Create dictionary where len(word) is key and word is value
+    # Create dictionary where len(word) is key and word is value.
     for word in words:
-        d[len(word)] = d.get(len(word), word).append(word)
-
-    # Create a sorted list tuples of the dictionary's keys/values
+        # Look for word's length as a key in d.
+        # If it's not there, insert it as a key and create an empty list for the value.
+        d[len(word)] = d.setdefault(len(word), [])
+        # Then append the word to the list (for some reason, chaining was throwing an error).
+        d[len(word)].append(word)
+    
+    # Create a sorted list tuples of the dictionary's keys/values.
     pairs = sorted(d.items())
 
+    # Initialize the empty list we're going to return soon.
     sorted_words = []
 
     # Append second item in each sorted tuple (i.e., the word)
     for pair in pairs:
-        sorted_words.append(pair[1])
+        for word in pair[1]:
+            print word
 
-    return sorted_words
+# print word_length(words)
 
-print word_length(words)
+# BONUS 1
+# Took 4 minutes
+def word_length2(filename):
+    d = {}
+    
+    fin = open(filename)
+
+    for line in fin:
+        line = line.replace("-", " ")
+        words = line.lower().split()
+        
+        # Create dictionary where len(word) is key and word is value.
+        for word in words:
+            word = word.strip(string.punctuation)
+            # Look for word's length as a key in d.
+            # If it's not there, insert it as a key and create an empty list for the value.
+            d[len(word)] = d.setdefault(len(word), [])
+            # Then append the word to the list (for some reason, chaining was throwing an error).
+            d[len(word)].append(word)
+    
+    # Create a sorted list tuples of the dictionary's keys/values.
+    pairs = sorted(d.items())
+
+    # Initialize the empty list we're going to return soon.
+    sorted_words = []
+
+    # Append second item in each sorted tuple (i.e., the word)
+    for pair in pairs:
+        for word in pair[1]:
+            print word
+
+#print word_length2("twain.txt")
+
+# BONUS 3
+# Took 2 minutes
+def word_length3(filename):
+    d = {}
+    
+    fin = open(filename)
+
+    for line in fin:
+        line = line.replace("-", " ")
+        words = line.lower().split()
+        
+        # Create dictionary where len(word) is key and word is value.
+        for word in words:
+            word = word.strip(string.punctuation)
+            # Look for word's length as a key in d.
+            # If it's not there, insert it as a key and create an empty list for the value.
+            d[len(word)] = d.setdefault(len(word), [])
+            # Then append the word to the list (for some reason, chaining was throwing an error).
+            d[len(word)].append(word)
+    
+    # Create a sorted list tuples of the dictionary's keys/values.
+    pairs = sorted(d.items())
+
+    # Initialize the empty list we're going to return soon.
+    sorted_words = []
+
+    # Append second item in each sorted tuple (i.e., the word)
+    for pair in pairs:
+        for word in sorted(pair[1]):
+            print word
+
+#print word_length3("twain.txt")
 
 """
 Here's a table of English to Pirate translations
@@ -173,3 +243,65 @@ man         matey
 Write a program that asks the user to type in a sentence and then
 print the sentence translated to pirate.
 """
+
+# Took 45 minutes
+glossary = """sir         matey
+hotel       fleabag inn
+student     swabbie
+boy         matey
+madam       proud beauty
+professor   foul blaggart
+restaurant  galley
+your        yer
+excuse      arr
+students    swabbies
+are         be
+lawyer      foul blaggart
+the         th'
+restroom    head
+my          me
+hello       avast
+is          be
+man         matey"""
+def pirate_translator(glossary):
+    
+    # Create empty dictionary.
+    d = {}
+
+    # Split glossary string on newlines into list of English and Pirate pairs
+    translations = glossary.split("\n")
+    
+    # Split each line at the double space mark
+    for line in translations:
+        line = line.split("  ")
+
+        # Some Pirate terms end up having a single space before them.
+        # This strips that extra space out.
+        if line[-1][0] == " ":
+            line[-1] = line[-1][1:]
+
+        # Add the first item of each line as the key
+        # and the last item in the list as the value.
+        d[line[0]] = line[-1]
+            
+    # Get input from user.
+    english = raw_input("Give me something to translate into Pirate: ")
+    
+    # Split it into a string.
+    english_list = english.split()
+
+    list_sans_punct = []
+
+    for word in english_list:
+        word = word.strip(string.punctuation)
+        list_sans_punct.append(word)
+    
+    translation = []
+    for word in list_sans_punct:
+        new_word = d.get(word, word)
+        translation.append(new_word)
+    return (" ").join(translation)
+
+
+print pirate_translator(glossary)
+
